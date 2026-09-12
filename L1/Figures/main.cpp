948af8fe09830f24cc051a78c48e5ce393f9cebd
GreatUstug <m.ustugoff@gmail.com>
2026-09-12 11:00:49 +0300

    Лабораторная 1. Фигуры. Создал проект

---
+++ L1/Figures/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by maxim on 11.09.2026.
+//
+
+#include <iostream>
+#include <ostream>
+
+int main()
+{
+	std::string x = "";
+	std::cin >> x;
+	std::cout << "Hello, " << x << std::endl;
+}
